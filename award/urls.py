2b49eab3b9b4b249564a41f1cc3dbc965de0a151
Gedihared6409 <gediali019@gmail.com>
2020-06-09 06:51:59 +0300

    addd urls for the profile

--- award/urls.py
+++ award/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns=[
     url('^$',views.home,name = 'home'),
     url(r'^post$', views.post, name='post'),
+    url(r'^accounts/profile/(\d+)', views.profile, name='profile'),
+    url(r'^accounts/edit_profile/', views.edit_profile, name='edit_profile'),
 
-]
 ## this references the location to the uploaded files.
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
