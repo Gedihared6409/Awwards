5cb545f3969833eb8090bd2a3fc1f02d52616a91
Gedihared6409 <gediali019@gmail.com>
2020-06-09 06:43:07 +0300

    initial commit

---
+++ award/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+from django.db import transaction
+from .models import Project,Profile,Review
+# Create your tests here.
+
---
+++ award/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+urlpatterns=[
+
+]
+## this references the location to the uploaded files.
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
