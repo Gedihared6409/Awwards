bcfb650e001c3b06da89b1dc2710cc84a77d6cfc
Gedihared6409 <gediali019@gmail.com>
2020-06-09 07:04:00 +0300

    createds models for the profie

--- award/models.py
+++ award/models.py
@@ -5,3 +5,17 @@ from tinymce.models import HTMLField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
+class Profile(models.Model):
+    photo=models.ImageField(upload_to='pic/')
+    bio=models.TextField()
+    username=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
+    phone_number=models.CharField(max_length=50)
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+       self.delete()
+
+    def update_bio(self,bio):
+         self.bio=bio
+         self.save()
+
