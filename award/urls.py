71408ee658a33283c8df5ff9a4f76375fe31235f
Gedihared6409 <gediali019@gmail.com>
2020-06-09 06:55:23 +0300

    added url for search

--- award/urls.py
+++ award/urls.py
@@ -7,7 +7,11 @@ urlpatterns=[
     url(r'^post$', views.post, name='post'),
     url(r'^accounts/profile/(\d+)', views.profile, name='profile'),
     url(r'^accounts/edit_profile/', views.edit_profile, name='edit_profile'),
-
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/profile/$', views.ProfileList.as_view(),name='profile'),
+    url(r'^api/project/$', views.ProjectList.as_view(),name='project'),
+    url(r'^grade/(\d+)',views.grade_project,name ='grade') ,
+]
 ## this references the location to the uploaded files.
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
