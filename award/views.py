63f284d10ad93544c67fa905948b89d1b3ee5905
Gedihared6409 <gediali019@gmail.com>
2020-06-09 06:44:44 +0300

    added base html and a function for displaying home page

--- award/views.py
+++ award/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,HttpResponseRedirect
 from .models import Project,Profile,Review
+from django.contrib.auth.decorators import login_required
+from .forms import NewPostForm,ProfileForm,GradeForm
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
+from rest_framework import status
+# Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    title='Awards'
+    post=Project.objects.all()
+    profile = Profile.objects.all()
+    grade= Review.objects.all()
+    current_user=request.user
+    return render(request,'AW/home.html',{'title':title ,'post':post,'profile':profile,'current_user':current_user,'grade':grade})
