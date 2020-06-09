dd4618bd62657b422ab09534a9045905a15de148
Gedihared6409 <gediali019@gmail.com>
2020-06-09 07:04:59 +0300

    createds models for the project

--- award/models.py
+++ award/models.py
@@ -18,4 +18,33 @@ class Profile(models.Model):
     def update_bio(self,bio):
          self.bio=bio
          self.save()
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+      if created:
+          Profile.objects.create(username=instance)
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+      instance.profile.save()
+class Project(models.Model):
+     title=models.CharField(max_length=100)
+     image=models.ImageField(upload_to = 'pic/')
+     description=HTMLField()
+     link= models.CharField(max_length=200)
+     profile=models.ForeignKey(Profile,null=True)
+     view_grade=models.IntegerField(null=True)
+     def save_project(self):
+         self.save()
+
+     def delete_project(self):
+       self.delete()
+       
+     def update_description(self,des):
+         self.description=des
+         self.save()
+     @classmethod
+     def search_project(cls,search_term):
+       project=cls.objects.filter(title__icontains=search_term)
+       return project 
+
 
