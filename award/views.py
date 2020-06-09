7ba0ca49f308a7bbc906065a2f78c15365afd916
Gedihared6409 <gediali019@gmail.com>
2020-06-09 06:48:17 +0300

    added a function that reloads profile in the views

--- award/views.py
+++ award/views.py
@@ -17,3 +17,11 @@ def home(request):
     grade= Review.objects.all()
     current_user=request.user
     return render(request,'AW/home.html',{'title':title ,'post':post,'profile':profile,'current_user':current_user,'grade':grade})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+   user_object = request.user
+   current_user = Profile.objects.get(username__id=request.user.id)
+   user = Profile.objects.get(username__id=id)
+   projects = Project.objects.all()
+   return render(request, "AW/profile.html", {"current_user":current_user,"projects":projects,"user":user,"user_object":user_object})
